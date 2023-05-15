4de20f2973c1cf03b213cd5ebd1152a29fd74f4d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-05-15 16:33:28 +0300

    changed file contacts

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,50 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
-const getContactById = async (contactId) => {}
+const updateListContacts = async (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const getAllContacts = await listContacts();
+  const result = getAllContacts.find(({ id }) => id === contactId);
+  return result || null;
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  const getAllContacts = await listContacts();
+  const index = getAllContacts.findIndex(({ id }) => id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = getAllContacts.splice(index, 1);
+  await updateListContacts(getAllContacts);
+  return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const getAllContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  getAllContacts.push(newContact);
+  await updateListContacts(getAllContacts);
+  return newContact;
+};
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
-}
+};
