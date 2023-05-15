58eaea05b3e684e00619a39071458366d6838c2a
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-05-15 19:30:16 +0300

    added code

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,52 @@
-const express = require('express')
+const express = require("express");
+const contacts = require("../../models/contacts");
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      const error = new Error("Not found");
+      error.status = 404;
+      throw error;
+      // return res.status(404).json({
+      //   message: "Not found",
+      // });
+    }
+    res.json(result);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({
+      message,
+    });
+    // res.status(500).json({
+    //   message: "Server error",
+    // });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
